55c61209d26ec67de154c00d54ff903082f8ec30
tama <tama@ttk1.net>
2021-03-10 01:04:26 +0900

    command-executor を使う方法で書き換えた(conoha server 部分だけ出来た)

--- conoha/cli.py
+++ conoha/cli.py
@@ -1,99 +1,132 @@
-import argparse
-
-def get_parser():
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest="subcommand")
+from executor import Command
 
 
-    # auth
-    auth = subparsers.add_parser('auth').add_subparsers(dest="subsubcommand")
-    auth_login = auth.add_parser('login')
-    auth_logout = auth.add_parser('logout')
+def nop(*args, **kwargs):
+    pass
 
 
-    # conoha server
-    server = subparsers.add_parser('server').add_subparsers(dest="subsubcommand")
+def get_command():
+    command = Command()
 
-    # conoha server create options...
-    # https://www.conoha.jp/docs/compute-create_vm.php
-    server_create = server.add_parser('create')
-    server_create.add_argument('--image-ref', help='image 参照先。対象 image の UUID を指定', required=True)
-    server_create.add_argument('--flavor-ref', help='VMプラン(flavor) の UUID を指定', required=True)
-    server_create.add_argument('--admin-pass', help='VMのrootパスワード')
-    server_create.add_argument('--key-name', help='SSHキーを利用する場合に指定する ')
-    server_create.add_argument('--security-groups', help='keyに”name“を、valueにセキュリティグループ名を指定する')
-    server_create.add_argument('--metadata', help='metadata の key:value ペア。')
-    server_create.add_argument('--instance-name-tag', help='ネームタグを入れる際に利用する。文字種：半角英数字、「 - 」、「 _ 」のみを許可。文字数：255文字以下, Default:VMに紐づくGlobalIPアドレス ')
-    server_create.add_argument('--block-device-mapping', nargs='+', help='deviceは１つのみマッピングできる')
-    server_create.add_argument('--volume-id', help='アタッチしたいVolumeのIDを指定する')
-    server_create.add_argument('--vnc-keymap', choices=['en-us', 'ja'], help='keymap 設定')
-    server_create.add_argument('--user-data', help='base64 encoded Cloud-Init script')
+    ########
+    # auth #
+    ########
 
+    command.subcommand('auth').subcommand('login').set_handler(nop)
+    command.subcommand('auth').subcommand('logout').set_handler(nop)
+
+    ##########
+    # server #
+    ##########
+
+    # conoha server create ...
+    command.subcommand('server').subcommand('create').add_argument(
+        '--image-ref', help='image 参照先。対象 image の UUID を指定', required=True
+    ).add_argument(
+        '--flavor-ref', help='VMプラン(flavor) の UUID を指定', required=True
+    ).add_argument(
+        '--admin-pass', help='VMのrootパスワード'
+    ).add_argument(
+        '--key-name', help='SSHキーを利用する場合に指定する '
+    ).add_argument(
+        '--security-groups', help='keyに”name“を、valueにセキュリティグループ名を指定する'
+    ).add_argument(
+        '--metadata', help='metadata の key:value ペア。'
+    ).add_argument(
+        '--instance-name-tag',
+        help='ネームタグを入れる際に利用する。文字種：半角英数字、「 - 」、「 _ 」のみを許可。文字数：255文字以下, Default:VMに紐づくGlobalIPアドレス '
+    ).add_argument(
+        '--block-device-mapping', nargs='+', help='deviceは１つのみマッピングできる'
+    ).add_argument(
+        '--volume-id', help='アタッチしたいVolumeのIDを指定する'
+    ).add_argument(
+        '--vnc-keymap', choices=['en-us', 'ja'], help='keymap 設定'
+    ).add_argument(
+        '--user-data', help='base64 encoded Cloud-Init script'
+    ).set_handler(nop)
     # conoha server start --server-id SERVER_ID
-    server_start = server.add_parser('start')
-    server_start.add_argument('--server-id', required=True)
-
+    command.subcommand('server').subcommand('start').add_argument(
+        '--server-id', required=True
+    ).set_handler(nop)
     # conoha server stop --server-id SERVER_ID
-    server_stop = server.add_parser('stop')
-    server_stop.add_argument('--server-id', required=True)
-
+    command.subcommand('server').subcommand('stop').add_argument(
+        '--server-id', required=True
+    ).set_handler(nop)
     # conoha server delete --server-id SERVER_ID
-    server_delete = server.add_parser('delete')
-    server_delete.add_argument('--server-id', required=True)
-
+    command.subcommand('server').subcommand('delete').add_argument(
+        '--server-id', required=True
+    ).set_handler(nop)
     # conoha server list
-    server_list = server.add_parser('list')
-
+    command.subcommand('server').subcommand('list').set_handler(nop)
     # conha server describe --server-id SERVER_ID
-    server_describe = server.add_parser('describe')
-    server_describe.add_argument('--server-id', required=True)
-
+    command.subcommand('server').subcommand('describe').add_argument(
+        '--server-id', required=True
+    ).set_handler(nop)
     # conoha sercer attach-port --server-id SERVER_ID --port-id PORT_ID
-    server_attach_port = server.add_parser('attach-port')
-    server_attach_port.add_argument('--server-id', required=True)
-    server_attach_port.add_argument('--port-id', required=True)
-
+    command.subcommand('server').subcommand('attach-port').add_argument(
+        '--server-id', required=True
+    ).add_argument(
+        '--port-id', required=True
+    ).set_handler(nop)
     # conoha sercer detach-port --server-id SERVER_ID --port-id PORT_ID
-    server_detach_port = server.add_parser('detach-port')
-    server_detach_port.add_argument('--server-id', required=True)
-    server_detach_port.add_argument('--port-id', required=True)
+    command.subcommand('server').subcommand('detach-port').add_argument(
+        '--server-id', required=True
+    ).add_argument(
+        '--port-id', required=True
+    ).set_handler(nop)
 
+    ##########
+    # subnet #
+    ##########
 
-    # subnet
-    subnet = subparsers.add_parser('subnet').add_subparsers()
-    subnet_create = subnet.add_parser('create')
-    subnet_delete = subnet.add_parser('delete')
-    subnet_describe = subnet.add_parser('describe')
+    # command.subcommand('subnet').subcommand('create').add_argument(
+    # ).set_handler(nop)
+    # command.subcommand('subnet').subcommand('delete').add_argument(
+    # ).set_handler(nop)
+    # command.subcommand('subnet').subcommand('describe').add_argument(
+    # ).set_handler(nop)
 
+    ##################
+    # security-group #
+    ##################
 
-    # security-group
-    security_group = subparsers.add_parser('security-group').add_subparsers()
-    security_group_create = security_group.add_parser('create')
-    security_group_delete = security_group.add_parser('delete')
-    security_group_list = security_group.add_parser('list')
-    security_group_describe = security_group.add_parser('describe')
+    # command.subcommand('security-group').subcommand('create').add_argument(
+    # ).set_handler(nop)
+    # command.subcommand('security-group').subcommand('delete').add_argument(
+    # ).set_handler(nop)
+    # command.subcommand('security-group').subcommand('list').add_argument(
+    # ).set_handler(nop)
+    # command.subcommand('security-group').subcommand('describe').add_argument(
+    # ).set_handler(nop)
 
+    ###########
+    # network #
+    ###########
 
-    # network
-    network = subparsers.add_parser('network').add_subparsers()
-    network_create = network.add_parser('create')
-    network_delete = network.add_parser('delete')
-    network_list = network.add_parser('list')
-    network_describe = network.add_parser('describe')
+    # command.subcommand('network').subcommand('create').add_argument(
+    # ).set_handler(nop)
+    # command.subcommand('network').subcommand('delete').add_argument(
+    # ).set_handler(nop)
+    # command.subcommand('network').subcommand('list').add_argument(
+    # ).set_handler(nop)
+    # command.subcommand('network').subcommand('describe').add_argument(
+    # ).set_handler(nop)
 
+    ########
+    # port #
+    ########
 
-    # port
-    port = subparsers.add_parser('port').add_subparsers()
-    port_create = port.add_parser('create')
-    port_delete = port.add_parser('delete')
-    port_list = port.add_parser('list')
-    port_describe = port.add_parser('describe')
+    # command.subcommand('port').subcommand('create').add_argument(
+    # ).set_handler(nop)
+    # command.subcommand('port').subcommand('delete').add_argument(
+    # ).set_handler(nop)
+    # command.subcommand('port').subcommand('list').add_argument(
+    # ).set_handler(nop)
+    # command.subcommand('port').subcommand('describe').add_argument(
+    # ).set_handler(nop)
 
-    return parser
+    return command
 
 
 def main():
-    parser = get_parser()
-    args = parser.parse_args()
-    print(vars(args))
-    #args.handler(**vars(args))
+    get_command().execute()
