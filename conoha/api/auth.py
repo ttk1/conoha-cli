abb233dc2ff68a972943f038f8315c080de8c64f
tama <tama@ttk1.net>
2021-03-15 00:16:03 +0900

    修正漏れ

--- conoha/api/auth.py
+++ conoha/api/auth.py
@@ -6,13 +6,12 @@ def auth_login():
     try:
         credential = config.get_credential()
     except FileNotFoundError:
-        with open(config.credential_path, mode='w', encoding='utf-8') as f:
-            credential = {
-                'user_name': input('Enter user name: '),
-                'password': getpass('Enter password: '),
-                'tenant_id': input('Enter tenant id: ')
-            }
-            config.save_credential(credential)
+        credential = {
+            'user_name': input('Enter user name: '),
+            'password': getpass('Enter password: '),
+            'tenant_id': input('Enter tenant id: ')
+        }
+        config.save_credential(credential)
 
     data = {
         'auth': {
