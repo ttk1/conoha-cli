4785d1777c82c03d6568e0ab40a8f01c3c5b5f19
tama <tama@ttk1.net>
2021-03-25 02:28:27 +0900

    JSON のデコードに失敗したら元の文字列をそのまま返すようにした + DELETE と PUT を追加

--- conoha/util/http.py
+++ conoha/util/http.py
@@ -6,12 +6,43 @@ def post(url, data, headers):
     req = urllib.request.Request(
         url, data=json.dumps(data).encode('utf-8'), headers=headers)
     with urllib.request.urlopen(req) as res:
-        body = res.read()
-        return json.loads(body.decode('utf-8'))
+        body = res.read().decode('utf-8')
+        try:
+            return json.loads(body)
+        except json.JSONDecodeError:
+            # JSON のデーコードに失敗したら文字列のまま返す
+            return body
 
 
 def get(url, headers):
     req = urllib.request.Request(url, headers=headers)
     with urllib.request.urlopen(req) as res:
-        body = res.read()
-        return json.loads(body.decode('utf-8'))
+        body = res.read().decode('utf-8')
+        try:
+            return json.loads(body)
+        except json.JSONDecodeError:
+            # JSON のデーコードに失敗したら文字列のまま返す
+            return body
+
+
+def delete(url, headers):
+    req = urllib.request.Request(url, headers=headers, method='DELETE')
+    with urllib.request.urlopen(req) as res:
+        body = res.read().decode('utf-8')
+        try:
+            return json.loads(body)
+        except json.JSONDecodeError:
+            # JSON のデーコードに失敗したら文字列のまま返す
+            return body
+
+
+def put(url, data, headers):
+    req = urllib.request.Request(
+        url, data=json.dumps(data).encode('utf-8'), headers=headers, method='PUT')
+    with urllib.request.urlopen(req) as res:
+        body = res.read().decode('utf-8')
+        try:
+            return json.loads(body)
+        except json.JSONDecodeError:
+            # JSON のデーコードに失敗したら文字列のまま返す
+            return body
